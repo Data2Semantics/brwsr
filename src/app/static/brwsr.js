9b17103f25ea42529cc373f5167bd4455e5c98f2
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-12-02 16:56:03 +0100

    Shorter literals

--- src/app/static/brwsr.js
+++ src/app/static/brwsr.js
@@ -2,14 +2,31 @@ $( document ).ready(function() {
  
     $(".resource").each(function(index){
       var element = this;
-      if ($(this).text() && $(this).text().substr(0,4) == 'http') {
+      var element_text = $(this).text();
+      if (element_text && element_text.substr(0,4) == 'http') {
         $.get('http://preflabel.org/api/v1/label/'+encodeURIComponent($(element).text())+"?callback=?", function(data){
           updateLabel(data,element);
         }).fail(function(){
-          updateLabel($(element).text(),element);
-          console.log("error");
+          updateLabel(element_text,element);
         });
       } 
+    });
+    
+    $(".literal").each(function(index){
+        var element = this;
+        var element_text = $(this).text();
+        if (element_text.length > 50 ) {
+
+              var span = $('<span></span>');
+
+              span.text(element_text.substr(0,200) + '...');
+              var icon = $('<span class="glyphicon glyphicon-play" aria-hidden="true"></span>');
+              span.append(icon);
+              icon.on('click', function(e){
+                  span.text(element_text);
+              });
+              $(element).html(span);
+          } 
     });
     
     function updateLabel(label,element){
