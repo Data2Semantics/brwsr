7f563eb914f3e5ef7541a07a7c4928aed21a6338
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-12-17 11:40:49 +0100

    Fixed port number

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -1,6 +1,6 @@
 workers = 1
 worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
-bind = '0.0.0.0:5444400'
+bind = '0.0.0.0:5400'
 pidfile = '/tmp/gunicorn-rivers.pid'
 debug = True
 loglevel = 'debug'
