3a49222341dff52bfb9afa729c69ca69811fe25c
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2017-02-24 09:59:29 +0100

    Added a waiting period

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@ echo "Updating from git"
 git pull
 echo "Killing existing gunicorn process"
 pkill -F /tmp/gunicorn-brwsr.pid
+echo "Waiting for a couple of seconds"
+sleep 3
 echo "Activating the virtual environment"
 source bin/activate
 echo "Changing directory to src"
