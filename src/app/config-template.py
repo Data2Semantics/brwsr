cb533fa7e7fb64490966518537b173c4ec3be786
Laurens Rietveld <laurens.rietveld@gmail.com>
2017-04-21 14:03:35 +0200

    use proper function call

--- src/app/config-template.py
+++ src/app/config-template.py
@@ -5,16 +5,16 @@ import os
 
 # Set LOCAL_STORE to True if you want brwsr to just load a (smallish) RDF file into server memory
 # rather than operate on an external SPARQL store
-LOCAL_STORE = os.getenv['LOCAL_STORE'] or False
+LOCAL_STORE = os.getenv('LOCAL_STORE') or False
 
 # Set LOCAL_FILE to the relative or absolute path of the file you want brwsr to load when
 # LOCAL_STORE is True. The brwsr application will just use RDFLib to guess the file format based on the extension.
 # You can use UNIX file masks such as * and ? to load multiple files
-LOCAL_FILE = os.getenv['LOCAL_FILE'] or 'justsomeexample.trig'
+LOCAL_FILE = os.getenv('LOCAL_FILE') or 'justsomeexample.trig'
 
 # Set this to the SPARQL endpoint uri of your triplestore
 # e.g. "http://dbpedia.org/sparql"
-SPARQL_ENDPOINT = os.getenv['SPARQL_ENDPOINT'] or "http://your.sparql.endpoint.here/sparql"
+SPARQL_ENDPOINT = os.getenv('SPARQL_ENDPOINT') or "http://your.sparql.endpoint.here/sparql"
 
 # If brwsr is backed by multiple separate triple stores, use SPARQL_ENDPOINT_MAPPING to
 # make sure that each URI for which the LOCAL_NAME (i.e. the URI with the DEFAULT_BASE remove, if present)
@@ -37,16 +37,16 @@ SPARQL_ENDPOINT_MAPPING = {}
 # the DEFAULT_BASE is *always* prepended to this local name to make up the URI that's used to
 # query the triple store
 # e.g. "http://dbpedia.org" (without the last slash!)
-DEFAULT_BASE = os.getenv['DEFAULT_BASE'] or "http://your.base.uri.here"
+DEFAULT_BASE = os.getenv('DEFAULT_BASE') or "http://your.base.uri.here"
 
 # The LOCAL_DOCUMENT_INFIX is the infix used between the DEFAULT_BASE and the local name of the URI
 # to denote the HTML representation of the RDF resource (see the Cool URI's specification)
-LOCAL_DOCUMENT_INFIX = os.getenv['LOCAL_DOCUMENT_INFIX'] or 'doc'
+LOCAL_DOCUMENT_INFIX = os.getenv('LOCAL_DOCUMENT_INFIX') or 'doc'
 
 # The LOCAL_SERVER_NAME is the address brwsr listens to. It needs to know this to build proper
 # requests when you click a URI in the brwsr page of a resource.
 # e.g. "http://localhost:5000" if running flask.
-LOCAL_SERVER_NAME = os.getenv['LOCAL_SERVER_NAME'] or "http://your.server.name.here"
+LOCAL_SERVER_NAME = os.getenv('LOCAL_SERVER_NAME') or "http://your.server.name.here"
 
 # By default brwsr assumes it is running at the root of the server,
 # If you want to run brwsr under a directory (e.g. http://example.com/brwsr rather than http://example.com), you need to do this
@@ -70,38 +70,38 @@ LOCAL_SERVER_NAME = os.getenv['LOCAL_SERVER_NAME'] or "http://your.server.name.h
 # The 'proxy_pass' setting should point to the address and port you are running brwsr at (default is localhost port 5000).
 #
 #########
-BEHIND_PROXY = os.getenv['BEHIND_PROXY'] or False
+BEHIND_PROXY = os.getenv('BEHIND_PROXY') or False
 
 
 # The START_LOCAL_NAME is the local name of the first URI shown in brwsr if no URI is specified
 # e.g. "resource/Amsterdam" when using the DBPedia settings
-START_LOCAL_NAME = os.getenv['START_LOCAL_NAME'] or "some/local/name"
+START_LOCAL_NAME = os.getenv('START_LOCAL_NAME') or "some/local/name"
 
 # The START_URI is simply the combination of the DEFAULT_BASE and the START_LOCAL_NAME
 # (i.e. there is no need to change this, usually)
 # e.g. this will become "http://dbpedia.org/resource/Amsterdam"
-START_URI = os.getenv['START_URI'] or urljoin(DEFAULT_BASE,START_LOCAL_NAME)
+START_URI = os.getenv('START_URI') or urljoin(DEFAULT_BASE,START_LOCAL_NAME)
 
 # Set query results limit because otherwise your browser might crash.
-QUERY_RESULTS_LIMIT = os.getenv['QUERY_RESULTS_LIMIT'] or 1000
+QUERY_RESULTS_LIMIT = os.getenv('QUERY_RESULTS_LIMIT') or 1000
 
 # The port via which to run brwsr
-PORT = os.getenv['PORT'] or 5000
+PORT = os.getenv('PORT') or 5000
 
 # Debug logging
-DEBUG = os.getenv['DEBUG'] or False
+DEBUG = os.getenv('DEBUG') or False
 
 # Browse URIs that do not match the DEFAULT_BASE
-BROWSE_EXTERNAL_URIS = os.getenv['BROWSE_EXTERNAL_URIS'] or True
+BROWSE_EXTERNAL_URIS = os.getenv('BROWSE_EXTERNAL_URIS') or True
 
 # Dereference external URIs (i.e. retrieve RDF served at that location, and display the resource)
 # NB: This may be slow, depending on the responsiveness of the server at hand
 # NB: The resulting RDF is stored locally (in memory) which means that this is a potential memory hog for
 # servers that are visited frequently. TODO: store results in a triple store
-DEREFERENCE_EXTERNAL_URIS = os.getenv['DEREFERENCE_EXTERNAL_URIS'] or False
+DEREFERENCE_EXTERNAL_URIS = os.getenv('DEREFERENCE_EXTERNAL_URIS') or False
 
 # Set the HTTP method to use for communicating with SPARQL endpoint. 'GET' is the default.
-SPARQL_METHOD = os.getenv['SPARQL_METHOD'] or 'GET'
+SPARQL_METHOD = os.getenv('SPARQL_METHOD') or 'GET'
 
 # Set any custom parameters to be sent to the SPARQL endpoint
 # e.g. CUSTOM_PARAMETERS = {'reasoning': 'true'} for Stardog
